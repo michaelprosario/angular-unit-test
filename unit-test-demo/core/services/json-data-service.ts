54c052352b512880b77aa2be1c7e51d80f7432d7
michaelprosario <michael.p.rosario@gmail.com>
2021-10-05 22:54:29 +0000

    Initial commit

---
+++ unit-test-demo/core/services/json-data-service.ts
@@ -0,0 +1,26 @@
+import { AbstractJsonRepository } from "core/interfaces/abstract-json-repository";
+
+export class JsonDataService
+{
+    constructor(private repository: AbstractJsonRepository){
+
+    }
+
+    public add(aObject: any, collection: string): string{
+        return "";
+    }
+
+    public delete(id: string, collection: string): string{
+        return "";
+    }
+
+    public get(id: string, collection: string): string{
+        return "";
+    }
+
+    public getAll(collection: string): Array<any>
+    {
+        return [];
+    }
+
+}
